5f8a1afdc79966daf8f1e7400f19d544db5d603a
Luis Cabrera Sauco <icorrecam@gmail.com>
2016-10-31 00:36:32 +0000

    Reconfigurando los distintos 'favicon'

--- js/agency.js
+++ js/agency.js
@@ -3,11 +3,11 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
-(function ($) {
+(function($) {
     'use strict';
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function (event) {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -22,7 +22,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function () {
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
         $('.navbar-toggle:visible').click();
     });
 
